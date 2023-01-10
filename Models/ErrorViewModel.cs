c4e126c56760f3ac126d160027f91aa4ea0b7da4
Yuvaraj I <yuvaraji@presidio.com>
2023-01-10 17:15:14 +0530

    Update removed used files

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace helloWorld.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
-
